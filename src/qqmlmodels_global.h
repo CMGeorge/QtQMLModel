6db4dd302b102ba6fd500269df04e192c3aa73c8
Calugar Mircea - George <george@wesell.ro>
2025-09-17 19:40:16 +0000

    Fix Windows DLL export/import for Qt MOC symbols
    - Updated qqmlmodels_global.h to use proper Qt-style export macros
    - Added QtCore/qglobal.h include for Q_DECL_EXPORT/Q_DECL_IMPORT
    - Removed CMake generate_export_header in favor of consistent custom header
    - Enhanced conditional compilation for Windows compatibility
    - Fixes LNK2019 unresolved external symbol errors on Windows
    - All tests passing on Linux, Windows compatibility verified

--- src/qqmlmodels_global.h
+++ src/qqmlmodels_global.h
@@ -1,14 +1,18 @@
 #ifndef QQMLMODELS_GLOBAL_H
 #define QQMLMODELS_GLOBAL_H
 
+#include <QtCore/qglobal.h>
+
 #ifndef QT_STATIC
 #if defined(QQML_EXPORT)
+/* We are building this library */
 #define QQMLMODELS_EXPORT Q_DECL_EXPORT
 #else
+/* We are using this library */
 #define QQMLMODELS_EXPORT Q_DECL_IMPORT
 #endif
 #else
 #define QQMLMODELS_EXPORT
 #endif
-// #define  QQMLMODELS_EXPORT
+
 #endif // QQMLMODELS_GLOBAL_H
