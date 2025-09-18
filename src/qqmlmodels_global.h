67fdc9e5d408351bf0615e3b0dab78a11167b577
Calugar Mircea - George <george@wesell.ro>
2025-09-18 08:31:44 +0000

    Fix Windows DLL export generation
    - Add generate_export_header() call to CMakeLists.txt for proper Windows DLL symbols
    - Update qqmlmodels_global.h to use generated export header instead of manual macros
    - This should resolve Windows CI linker errors for Qt MOC staticMetaObject symbols
    - Tested locally on Linux: all tests pass

--- src/qqmlmodels_global.h
+++ src/qqmlmodels_global.h
@@ -3,16 +3,10 @@
 
 #include <QtCore/qglobal.h>
 
-#ifndef QT_STATIC
-#if defined(QQML_EXPORT)
-/* We are building this library */
-#define QQMLMODELS_EXPORT Q_DECL_EXPORT
-#else
-/* We are using this library */
-#define QQMLMODELS_EXPORT Q_DECL_IMPORT
-#endif
-#else
-#define QQMLMODELS_EXPORT
-#endif
+// Use CMake-generated export header for proper Windows DLL symbol export
+#include "cppqmlmodels_export.h"
+
+// Map the generated export macro to our library's export macro name
+#define QQMLMODELS_EXPORT CPPQMLMODELS_EXPORT
 
 #endif // QQMLMODELS_GLOBAL_H
